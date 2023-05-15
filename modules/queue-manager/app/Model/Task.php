3631e75db6d328c482bc5a197b12193a3ac5cce9
shaho1090 <shaho.parivni@gmail.com>
2023-05-15 23:48:19 +0330

    add task table and model

---
+++ modules/queue-manager/app/Model/Task.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace QueueManager\Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'payload',
+        'status'
+    ];
+}
