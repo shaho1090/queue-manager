b7170838995d0294c06083706d986dbb979d9960
shaho1090 <shaho.parivni@gmail.com>
2023-05-16 22:17:28 +0330

    add queue manager service provider

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        QueueManager\Providers\QueueManagerServiceProvider::class
     ])->toArray(),
 
     /*
